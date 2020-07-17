723b49e236425b9e337191d995ad6252d6577aa8
Steve Taranto <staranto@users.noreply.github.com>
2020-07-17 10:09:05 -0400

    Re-added ppc64le and s390x so we can experiment.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 baseimage="alpine"
-dockerarch=("amd64" "arm32v6" "arm64v8")
-goarch=("amd64" "arm"  "arm64")
+dockerarch=("amd64" "arm32v6" "arm64v8" "ppc64le" "s390x")
+goarch=("amd64" "arm" "arm64" "ppc64le" "s390x")
 
 for i in "${!dockerarch[@]}"; do
     fromimage="${dockerarch[$i]}/${baseimage}"
